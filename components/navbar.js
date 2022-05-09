7e5c6397579c7e34f6915e7bdad9980ad7fcc20b
afzal9632 <mdafzal9632@gmail.com>
2022-05-09 21:12:40 +0530

    food app 2

---
+++ components/navbar.js
@@ -0,0 +1,11 @@
+let navbar= ()=>{
+return` <div id="navbar">
+<div><a href="index.html"> Search receipe by name</a></div>
+<div> <a href="day.html">Get receipe of the day</a></div>
+<div><a href="random.html">Show random receipes</a></div>   
+</div>
+<div id="search_bar">
+<input type="text" id="search" placeholder="Search Food"/>
+</div>`
+}
+export{navbar}
